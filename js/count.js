25d6d4926bb5bd503d9390af689a5ce8f9cd936b
luvuong-le <lu-vuongle@hotmail.com>
2018-05-02 23:26:50 +1000

    Added Stop and Reset Button and fixed issues with negative values

--- js/count.js
+++ js/count.js
@@ -7,7 +7,11 @@ let milli = document.getElementById("milli");
 
 function updateClock() {
     let currentDateandTime = new Date();
-    hours.innerHTML = currentDateandTime.getHours() - 12 + ":";
+    if (currentDateandTime.getHours() < 12) {
+        hours.innerHTML = currentDateandTime.getHours() + ":";
+    } else {
+        hours.innerHTML = currentDateandTime.getHours() - 12 + ":";
+    }
     if (currentDateandTime.getMinutes() < 10) {
         minute.innerHTML = "0" + currentDateandTime.getMinutes() + ":";
     } else {
@@ -22,6 +26,12 @@ let time = setInterval(updateClock, 200);
 /* Countdown Timer */
 let submitBtn = document.getElementById("submitBtn");
 
+/* Stop Button */
+let stopBtn = document.getElementById("stopBtn");
+
+/* Reset Button */
+let resetBtn = document.getElementById("resetBtn");
+
 /* Form Values */
 let formHour = document.getElementById("countHour");
 let formMin = document.getElementById("countMin");
@@ -32,12 +42,54 @@ let CDHour = document.getElementById("count-hour-value");
 let CDMin = document.getElementById("count-min-value");
 let CDSeconds = document.getElementById("count-second-value");
 
-submitBtn.addEventListener("click", (e) => {
-    /* Begin the count down */
+let intervals = [];
+
+function clearIntervals() {
+    intervals.forEach(function(interval) {
+        clearInterval(interval);
+    });
+}
+
+function resetFormValues() {
+    formHour.value = "";
+    formMin.value = "";
+    formSeconds.value = "";
+}
+
+function resetCountDownDisplay() {
+    CDHour.innerHTML = "00:";
+    CDMin.innerHTML = "00:";
+    CDSeconds.innerHTML = "00";
+}
+
+resetBtn.addEventListener("click", e => {
+    e.preventDefault();
+
+    clearIntervals();
+
+    resetFormValues();
+
+    resetCountDownDisplay();
+});
+
+stopBtn.addEventListener("click", (e) => {
 
     e.preventDefault();
 
-    let countdown = setInterval(function() {
+    clearIntervals();
+});
+
+submitBtn.addEventListener("click", (e) => {
+    /* Begin the count down */
+
+    if (formHour.value <= 12 && formHour.value !== "" && 
+        formMin.value <= 60 && formMin.value !== "" && 
+        formSeconds.value <= 60 && formSeconds.value !== "") {
+        e.preventDefault();
+
+        clearIntervals();
+
+        let countdown = setInterval(function() {
         formSeconds.value--;
 
         if (formHour.value == 0 && formMin.value == 0 && formSeconds.value == 0) {
@@ -46,69 +98,69 @@ submitBtn.addEventListener("click", (e) => {
             console.log("Countdown Finished");
         } else {
             /* If the seconds reach 0, -1 from minutes and put seconds back to 60 */
-            if (formSeconds.value == 0) {
-                formMin.value--;
-                formSeconds.value = 59;
+            if (formSeconds.value == -1) {
+            formMin.value--;
+            formSeconds.value = 59;
             }
 
-            if (formMin.value == 0 && formHour.value != 0) {
-                formHour.value--;
-                formMin.value = 59;
+            if (formMin.value == -1 && formHour.value != 0) {
+            formHour.value--;
+            formMin.value = 59;
             }
 
             /* Set the values */
             if (formHour.value < 10) {
-                CDHour.innerHTML = "0" + formHour.value + ":";
+            CDHour.innerHTML = "0" + formHour.value + ":";
             } else {
-                CDHour.innerHTML = formHour.value + ":";
+            CDHour.innerHTML = formHour.value + ":";
             }
 
             if (formMin.value < 10) {
-                CDMin.innerHTML = "0" + formMin.value + ":";
+            CDMin.innerHTML = "0" + formMin.value + ":";
             } else {
-                CDMin.innerHTML = formMin.value + ":";
+            CDMin.innerHTML = formMin.value + ":";
             }
 
             if (formSeconds.value < 10) {
-                CDSeconds.innerHTML = "0" + formSeconds.value;
+            CDSeconds.innerHTML = "0" + formSeconds.value;
             } else {
-                CDSeconds.innerHTML = formSeconds.value;
+            CDSeconds.innerHTML = formSeconds.value;
             }
 
             /* If the seconds reach 0, -1 from minutes and put seconds back to 60 */
             if (formSeconds.value == 0) {
-                formMin.value--;
-                formSeconds.value = 59;
+            formMin.value--;
+            formSeconds.value = 59;
             }
 
             if (formMin.value == 0 && formHour.value != 0) {
-                formHour.value--;
-                formMin.value = 59;
+            formHour.value--;
+            formMin.value = 59;
             }
 
             /* Set the values */
             if (formHour.value < 10) {
-                CDHour.innerHTML = "0" + formHour.value + ":";
+            CDHour.innerHTML = "0" + formHour.value + ":";
             } else {
-                CDHour.innerHTML = formHour.value + ":";
+            CDHour.innerHTML = formHour.value + ":";
             }
 
             if (formMin.value < 10) {
-                CDMin.innerHTML = "0" + formMin.value + ":";
+            CDMin.innerHTML = "0" + formMin.value + ":";
             } else {
-                CDMin.innerHTML = formMin.value + ":";
+            CDMin.innerHTML = formMin.value + ":";
             }
 
             if (formSeconds.value < 10) {
-                CDSeconds.innerHTML = "0" + formSeconds.value;
+            CDSeconds.innerHTML = "0" + formSeconds.value;
             } else {
-                CDSeconds.innerHTML = formSeconds.value;
-            }   
+            CDSeconds.innerHTML = formSeconds.value;
+            }
         }
-        
-        console.log(formHour.value + " hours and " + formMin.value + " minutes and " + formSeconds.value + " seconds left");
 
-       
-    }, 1000);
+            console.log(formHour.value + " hours and " + formMin.value + " minutes and " + formSeconds.value + " seconds left");
+        }, 1000);
 
+        intervals.push(countdown);
+    }
 });
